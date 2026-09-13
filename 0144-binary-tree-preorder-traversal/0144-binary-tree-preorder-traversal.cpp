6453a3b91b291309bdc6495521b4744471231b92
harshksinghal2004-svg <harshksinghal2004@gmail.com>
2026-09-13 22:52:59 +0530

    Time: 0 ms (100%), Space: 11 MB (16.97%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,27 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
         vector<int> ans;
+        stack<TreeNode*> st;
         if (root == nullptr)
             return ans;
         st.push(root);
         while (!st.empty()) {
-            TreeNode* a = st.top();
-            ans.push_back(a->val);
+            TreeNode* temp = st.top();
+            ans.push_back(temp->val);
             st.pop();
-            if(a->right!=nullptr)
-            st.push(a->right);
-              if(a->left!=nullptr)
-            st.push(a->left);
-           
+            if (temp->right != nullptr)
+                st.push(temp->right);
+            if (temp->left != nullptr)
+                st.push(temp->left);
         }
-        return ans ;
+        return ans;
     }
 };
